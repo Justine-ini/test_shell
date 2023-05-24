82e811c135c9df783e6339f1cece188165bebc9f
Justine-ini <inijustine4040@gmail.com>
2023-05-24 14:25:02 -0700

    shell.c file

--- shell.c
+++ shell.c
@@ -1,10 +1,18 @@
 #include "shell.h"
+/**
+ * main - Entry point of the program
+ * @ac: The number of command-line arguments
+ * @av: Array of command-line arguments
+ * @env: Array of environment variables
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(int ac, char **av, char **env)
 {
-	if (ac == 1)
-	{
-		prompt(av, env);
-	}
-	return (0);
+        if (ac == 1)
+        {
+                prompt(av, env);
+        }
+        return (0);
 }
