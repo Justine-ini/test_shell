cbcbbe202cc718cb957668f86622e752a6734219
Justine-ini <inijustine4040@gmail.com>
2023-05-24 14:31:15 -0700

    header file

--- shell.h
+++ shell.h
@@ -5,8 +5,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 
 void prompt(char **av, char **env);
+char *_getenv(const char *name, char **env);
+char *search_in_path(const char *command, const char *path);
+int find_command(const char *command, char **env, char *command_path);
+char *build_path(char *dir, char *command);
+void print_message(void);
 
 #endif
